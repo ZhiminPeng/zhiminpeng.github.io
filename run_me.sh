9125b5fc7053a64e24d72f160346b8de48da81d7
Zhimin Peng <zhiminp@gmail.com>
2016-06-06 17:43:52 -0700

    removed the arock docs, added new tabs

--- run_me.sh
+++ run_me.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #compile the jemdoc files
+source ~/.bashrc
 
-jemdoc -c mysite.conf index paper MENU cv ms164 software pic10a15s
+jemdoc -c mysite.conf index paper MENU cv ms164 software pic10a15s blog
 
 rm *~
